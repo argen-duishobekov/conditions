4cc56675eef47f032729643f6a8289103653f1b7
Argen <dui.argesh96@gmail.com>
2021-03-23 17:39:06 +0300

    initial commit

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import './Card.css'
+
+export const Card = ({data}) => {
+ 
+
+        // if(data.color) {
+          // return  <div style={{background:data.color}} className = "card">
+            {/* {data.title} */}
+          {/* </div> */}
+        // }else {
+          // return  <div style={{backgroundImage:`url(${data.img})`}} className = "card">
+            {/* <p> */}
+            {/* карточка с рисунка */}
+            {/* </p> */}
+{/*              */}
+            {/* {data.title} */}
+          {/* </div> */}
+        // }
+
+        // switch (data.color) {
+          // case null:
+            // return  <div style={{backgroundImage:`url(${data.img})`}} className = "card">
+            {/* <p> */}
+            {/* карточка с рисунка */}
+            {/* </p> */}
+            {/* {data.title} */}
+            {/* </div> */}
+        // default:
+          // return  <div style={{background:data.color}} className = "card">
+          {/* {data.title} */}
+        {/* </div> */}
+      //  
+        // } 
+
+      //  return <>
+       {/* { */}
+              // data.color
+              // ?<div style={{background:data.color}} className = "card">
+              {/* {data.title} */}
+               {/* </div> */}
+        //  :  <div style={{backgroundImage:`url(${data.img})`}} className = "card">
+         {/* <p> */}
+         {/* карточка с рисунка */}
+         {/* </p> */}
+         {/* {data.title} */}
+         {/* </div> */}
+          //  
+          //  
+      //  }
+       {/* </> */}
+       
+    //  return <> 
+     {/* { */}
+          // data.color && <div style={{background:data.color}} className = "card">
+          {/* {data.title} */}
+           {/* </div> */}
+    //  }
+     {/* { */}
+        //  data.color === null && <div style={{backgroundImage:`url(${data.img})`}} className = "card">
+         {/* <p> */}
+         {/* карточка с рисунка */}
+         {/* </p> */}
+         {/* {data.title} */}
+         {/* </div> */}
+    //  }
+     {/* </> */}
+ 
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
